36a62b990d5bbab568ebbb9ab34734fadcebafad
williain <williain@users.noreply.github.com>
2015-04-04 21:34:29 +0100

    Adding check that you've installed the module
    Checking that you've installed the module correctly at the current
    version, so you can be sure your failing tests are failing for valid
    reasons, and not because you forgot to reinstall after the last hacking
    session (again).

---
+++ test_check.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import sys
+import os
+import os.path
+MODULE='multitools'
+
+installs=[entry for entry in sys.path if os.path.exists(os.path.join(entry, 'multitools'))]
+if len(installs)==0:
+    print("Module hasn't been installed; Run setup.py install first")
+    exit(1)
+install=os.path.join(installs[0],MODULE)
+
+def diff(localpath,installpath):
+    for fil in os.listdir(localpath):
+        localfil=os.path.join(localpath, fil)
+        if os.path.isdir(localfil):
+            if not diff(localfil, os.path.join(installpath,fil)):
+                return False
+        elif fil.endswith('.py'):
+            installfil=os.path.join(installpath,fil)
+            if os.path.getsize(localfil) != os.path.getsize(installfil):
+                return False
+            else:
+                lfil=open(localfil)
+                ifil=open(installfil)
+                for line in lfil:
+                    if line != ifil.readline():
+                        lfil.close()
+                        ifil.close()
+                        return False
+    return True
+
+if not diff('src',install):
+    print("Module installed is at a different version to local module;")
+    print("Run setup.py install again")
+    exit(1)
+else:
+    exit(0)
+
